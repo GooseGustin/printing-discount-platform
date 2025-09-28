45bd31ffbd03530c3a873e97c5c521d37165e6ea
GooseGustin <pammafeng11@gmail.com>
2025-09-28 02:35:16 +0100

    Deployment fix: changed all relative paths to begin from src again again

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -1,13 +1,13 @@
 import { SequelizeModule } from "@nestjs/sequelize";
+import { Module } from "@nestjs/common";
 import { TransactionsController } from "./transactions.controller";
 import { TransactionsService } from "./transactions.service";
-import { Transaction } from "@/models/transaction.model";
-import { Module } from "@nestjs/common";
+import { Transaction } from "../../models/transaction.model";
 import { ReceiptsService } from "../receipts/receipts.service";
 import { ReceiptsController } from "../receipts/receipts.controller";
-import { Receipt } from "@/models/receipt.model";
-import { User } from "@/models/user.model";
-import { Plan } from "@/models/plan.model";
+import { Receipt } from "../../models/receipt.model";
+import { User } from "../../models/user.model";
+import { Plan } from "../../models/plan.model";
 import { CalculationsModule } from "../calculations/calculations.module";
 
 // transactions.module.ts
